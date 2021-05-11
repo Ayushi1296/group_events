ef53d2c4fdbfc2ba6816e87c16529db6b87e8a73
Ayushi Jain <ayushi.sagi96@gmail.com>
2021-05-11 20:02:24 +0530

    refining

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
 # frozen_string_literal: true
-
-if User.count.zero?
-  User.create!(
-    name: 'test user',
-    email: 'test@email.com'
-  )
-end
+User.create( name: 'test user', email: 'test@email.com' )
